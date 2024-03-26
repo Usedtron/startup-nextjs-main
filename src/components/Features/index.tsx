19042551048aaa41d6d2b888e12aad2ca93f9ed6
Nicolai <usedtron@gmail.com>
2024-03-26 22:00:52 +0000

    new project

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,27 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section id="features" className="py-16 md:py-20 lg:py-28">
+        <div className="container">
+          <SectionTitle
+            title="SER NOSSO CLIENTE"
+            paragraph="Vai ter ao seu dispor uma forte estrutura de apoio, bem como todas as ferramentas necessárias para que possa ser bem sucedido no seu negócio."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
