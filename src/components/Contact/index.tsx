8de0c0e6eff534eb3c064b5dcc85f6d622d543a8
Nicolai <usedtron@gmail.com>
2024-03-31 14:57:03 +0100

    upgrade contactos

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -14,9 +14,7 @@ const Contact = () => {
               <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
                 Entre em Contacto
               </h2>
-              <p className="mb-12 text-base font-medium text-body-color">
-                
-              </p>
+              <p className="mb-12 text-base font-medium text-body-color"></p>
               <form>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
@@ -49,6 +47,23 @@ const Contact = () => {
                       />
                     </div>
                   </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="phoneNumber"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Numero de Telefone
+                      </label>
+                      <input
+                        type="tel"
+                        placeholder="Escreva o numero"
+                        pattern="[0-9]*" 
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+
                   <div className="w-full px-4">
                     <div className="mb-8">
                       <label
