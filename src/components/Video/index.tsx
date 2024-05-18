adaba4b83409469f31825429b1a82f652568c607
Nicolai <usedtron@gmail.com>
2024-05-18 22:01:45 +0100

    fix some bugs

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -26,39 +26,18 @@ const Video = () => {
               data-wow-delay=".15s"
             >
               <div className="relative aspect-[77/40] items-center justify-center">
-                <Image src="/images/video/video.jpg" alt="video image" fill />
-                <div className="absolute right-0 top-0 flex h-full w-full items-center justify-center">
-                  <button
-                    aria-label="video play button"
-                    onClick={() => setOpen(true)}
-                    className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white bg-opacity-75 text-primary transition hover:bg-opacity-100"
-                  >
-                    <svg
-                      width="16"
-                      height="18"
-                      viewBox="0 0 16 18"
-                      className="fill-current"
-                    >
-                      <path d="M15.5 8.13397C16.1667 8.51888 16.1667 9.48112 15.5 9.86602L2 17.6603C1.33333 18.0452 0.499999 17.564 0.499999 16.7942L0.5 1.20577C0.5 0.43597 1.33333 -0.0451549 2 0.339745L15.5 8.13397Z" />
-                    </svg>
-                  </button>
-                </div>
+              <iframe
+              loading="lazy"
+              style={{ position: "absolute", width: "100%", height: "10n0%", top: 0, left: 0, border: "none", padding: 0, margin: 0 }}
+              src="https://www.canva.com/design/DAGFPaviRLU/uQnU-Yix9if2JiEPO8R1kQ/watch?embed"
+              allowFullScreen="true"
+            />
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <ModalVideo
-        channel="youtube"
-        autoplay={true}
-        start={true}
-        isOpen={isOpen}
-        videoId="L61p2uyiMSo"
-        onClose={() => setOpen(false)}
-      />
-
-      <div className="absolute bottom-0 left-0 right-0 z-[-1] h-full w-full bg-[url(/images/video/shape.svg)] bg-cover bg-center bg-no-repeat"></div>
     </section>
   );
 };
